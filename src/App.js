25c532eb8d6d90ab85bbbc33edd968a7b20a668b
Zhi He <zhihe122@gmail.com>
2022-07-23 01:03:12 -0700

    separated some JSX into components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import "./App.css";
-
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 // The functional component is the same as the class component since they are rendering the same thing. However, in class component, you tell react specifically what you want react to render by overridding the render() method.
 class App extends Component {
   // constructor is always called first and build the foundation for the class with attributes.
@@ -14,56 +15,57 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-
-    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("componentDidMount");
     //Since the HTTP response is not a JSON Object,
     //.json() extracts the JSON Object from the HTTP response
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        );
+        this.setState(() => {
+          return { monsters: users };
+        });
       });
   }
 
-  render() {
-    console.log("render");
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    this.setState(() => {
+      return {
+        searchField,
+      };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-        <input
+        {/* <input
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLowerCase();
-
-            this.setState(() => {
-              return {
-                searchField,
-              };
-            });
-          }}
-        />
-        {filteredMonsters.map((monster) => (
+          onChange={onSearchChange}
+        /> */}
+        {/* {filteredMonsters.map((monster) => (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>
-        ))}
+        ))} */}
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+          className="search-box"
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
